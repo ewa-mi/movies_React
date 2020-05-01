958d1af69b74326aa3e51da0c4fd30163d5ffc87
ewami <efili1989@gmail.com>
2020-05-01 18:09:24 +0200

    test

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          NEW TEXT{" "}
+          very new TEXT
         </a>
       </header>
     </div>
